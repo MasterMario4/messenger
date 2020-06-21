7fe1d49765b52f200f0b46e4afb2d56f77c39071
MasterMario4 <mastermario4@gmail.com>
2020-06-21 11:06:06 +0200

    Added message logging to console.

--- src/main/java/pl/sda/messenger/frontend/FrontendController.java
+++ src/main/java/pl/sda/messenger/frontend/FrontendController.java
@@ -1,17 +1,30 @@
 package pl.sda.messenger.frontend;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.sda.messenger.frontend.dto.SendMessageDto;
 
 import java.time.LocalTime;
 
+@Log
 @Controller
 public class FrontendController {
 
     @GetMapping("/")
-    public String index(final Model model){
+    public String index(final Model model) {
         model.addAttribute("time", LocalTime.now());
+        model.addAttribute("newMessage", new SendMessageDto());
         return "index";
+    }
+
+    @PostMapping("/")
+    public String receiveMessage(final Model model, @ModelAttribute SendMessageDto messageDto) {
+        log.info("Message received: " + messageDto);
+
+        return "redirect:";
     }
 }
---
+++ src/main/java/pl/sda/messenger/frontend/dto/SendMessageDto.java
@@ -0,0 +1,10 @@
+package pl.sda.messenger.frontend.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SendMessageDto {
+    private String message;
+}
